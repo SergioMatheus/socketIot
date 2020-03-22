17dc30ef0f6de8a73722f11d50f4deb647bf0ad9
Sérgio Matheus <matheus_imagina@hotmail.com>
2020-03-22 16:53:31 -0300

    Update Cliente.java

--- src/principal/Cliente.java
+++ src/principal/Cliente.java
@@ -1,29 +1,39 @@
 package principal;
 
-import java.io.ObjectInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.SocketException;
 
 public class Cliente {
-
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		try {
-			Socket cliente = new Socket("10.8.172.14", 8082);
-			ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-//			PrintWriter saida = new PrintWriter(cliente.getOutputStream(),true);
-			String mensagemDoServidor = (String) entrada.readObject();
-			System.out.println(mensagemDoServidor);
-			Scanner sc = new Scanner(cliente.getInputStream());
-			while(sc.hasNextLine()) {
-//				saida.flush();
-//				saida.print(sc.nextLine());
-			}
-//			cliente.close();
-//			System.out.println("Conexão encerrada");
-		} catch (Exception e) {
-			System.out.println("Erro: " + e.getMessage());
-		}
+			String host = null;
+			int porta = 0;
+			System.out.println("Rodando CÃ³digo");
+			host = "localhost";
+			porta = 8082;
 
+			Socket socket = new Socket(host, porta);
+			BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String responseLine = is.readLine();
+			System.out.println("Mensagem do Servidor: " + responseLine);
+
+			while (true) {
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+				String userInput = userInputBR.readLine();
+				if (userInput != null) {
+					out.flush();
+					out.println(userInput + "\r\n");
+				}
+			}
+		} catch (SocketException e) {
+			System.out.println("!!Conexao Encerrada!!");
+		} catch (IOException e) {
+			System.out.println("Erro de conexao");
+		}
 	}
 }
